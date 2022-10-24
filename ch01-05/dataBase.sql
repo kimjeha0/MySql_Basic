b0f489c2b2b2c9bd5e7473fea231d0e904dff46d
김제하 <kimjeha0@daum.net>
2022-10-24 13:01:43 +0000

    데이터 베이스 기초

---
+++ ch01-05/dataBase.sql
@@ -0,0 +1,14 @@
+#생성된 데이터 베이스 뭐가 있는 지 읽어내기
+SHOW DATABASES; 
+
+#데이터 베이스 생성하기(persons_db)
+CREATE DATABASE persons_db;#데이터 베이스 이름이 persons_db
+
+#데이터 베이스 사용하기
+USE persons_db;
+
+#사용하는 데이터 베이스 읽어내기
+SELECT DATABASE();
+
+
+
